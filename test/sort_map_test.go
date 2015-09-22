adbb772f4e3859044a4e6cca5c42c1cc1c51494d
flybywind@foxmail.com <flybywind@foxmail.com>
2015-09-22 18:08:30 +0800

    finish test

--- test/sort_map_test.go
+++ test/sort_map_test.go
@@ -36,5 +36,29 @@ func TestStringKey(t *testing.T) {
 	ps := sort_map.NewPairSlice(test_map)
 	sort_map.By(by).Sort(ps)
 
-	fmt.Println(ps)
+	fmt.Printf("%s\n", ps.String())
+}
+
+func TestIntKey(t *testing.T) {
+	src := rand.NewSource(123456)
+	r := rand.New(src)
+	dict := []byte("abcxyz")
+	const (
+		key_len = 4
+		arr_num = 10
+	)
+	test_map := map[string]int{}
+	for i := 0; i < arr_num; i++ {
+		key := makeRandStr(r, dict, key_len)
+		test_map[key] = r.Intn(1000)
+	}
+
+	by := func(p1, p2 *sort_map.MapPair) bool {
+		return p1.ValueCmp(p2)
+	}
+
+	ps := sort_map.NewPairSlice(test_map)
+	sort_map.By(by).Sort(ps)
+
+	fmt.Printf("%s\n", ps.String())
 }
